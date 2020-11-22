6cec0a2b6b1d37942de6b90916bd20d003c90167
megatrust <tarekalabd1@gmail.com>
2020-11-22 12:23:13 +0200

    number trivia page

--- lib/features/number_trivia/presentation/pages/number_trivia_page.dart
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:number_trivia/features/number_trivia/presentation/widgets/widgets.dart';
+import 'package:number_trivia/injection_container.dart';
 
 class NumberTriviaPage extends StatelessWidget {
   @override
@@ -9,9 +12,44 @@ class NumberTriviaPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Number Trivia'),
       ),
-      body: BlocProvider(
-        builder: (_) => NumberTriviaBloc(),
-        child: Container(),
+      body: SingleChildScrollView(child: buildBody(context)),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      builder: (_) => sl<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: 10.0),
+              // tOp half of the screen
+
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is Empty) {
+                    return MessageDisplay(
+                      message: 'Start searching!',
+                    );
+                  } else if (state is Loading) {
+                    return LoadingWidget();
+                  } else if (state is Loaded) {
+                    return TriviaDisplay(numberTrivia: state.trivia);
+                  } else if (state is Error) {
+                    return MessageDisplay(message: state.message);
+                  }
+                  return Container();
+                },
+              ),
+              SizedBox(height: 20.0),
+
+              // Bottom half
+              TriviaControls(),
+            ],
+          ),
+        ),
       ),
     );
   }
